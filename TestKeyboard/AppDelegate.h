0f42cd8694e521d02a698df8f8b4d1e5f742a380
Jocelyn Sussman <jocelynsussman@yahoo.com>
2016-04-23 14:07:20 -0500

    Initial Commit

---
+++ TestKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestKeyboard
+//
+//  Created by Jocelyn Sussman on 4/23/16.
+//  Copyright © 2016 Jacob Sussman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestKeyboard
+//
+//  Created by Jocelyn Sussman on 4/23/16.
+//  Copyright © 2016 Jacob Sussman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
